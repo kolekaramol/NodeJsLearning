e95d2bcdad7bae5b127b4867cf930f15b1367403
unknown <kolekaramol786786@gmail.com>
2020-07-18 18:48:38 +0530

    day1 changes

--- day1chalk/day1chalk.js
+++ day1chalk/day1chalk.js
@@ -1,4 +1,6 @@
-const chalk=require('chalk');
+day1chalk()
+{
+    const chalk=require('chalk');
 const log=console.log;
 log(chalk.blue('Welcome'));
 log(chalk.white.bgRed.bold('Hello world!'));
@@ -6,3 +8,6 @@ log(`Student Marks
 Amol : ${chalk.white.bgRedBright.bold('(95 %)')}
 Anwar : ${chalk.white.bgGreen.bold('(78 %)')}
 `)
+
+}
+module.export=day1chalk
---
+++ index.js
@@ -0,0 +1,2 @@
+const day1Chalk=require('./day1chalk/day1chalk')
+day1Chalk.day1chalk();
