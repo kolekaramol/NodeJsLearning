55dc2cb0d78df616b358d255a0a6f221f13b203c
unknown <kolekaramol786786@gmail.com>
2020-07-18 17:49:58 +0530

    first commit

---
+++ day1chalk/day1chalk.js
@@ -0,0 +1,8 @@
+const chalk=require('chalk');
+const log=console.log;
+log(chalk.blue('Welcome'));
+log(chalk.white.bgRed.bold('Hello world!'));
+log(`Student Marks
+Amol : ${chalk.white.bgRedBright.bold('(95 %)')}
+Anwar : ${chalk.white.bgGreen.bold('(78 %)')}
+`)
